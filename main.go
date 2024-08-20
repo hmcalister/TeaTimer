8aa7f59e79f8083839d75667045f5f5321a31f48
Hayden McAlister <haydenmcalister49@gmail.com>
2024-08-21 00:32:25 +1200

    Update main go with some testing

--- main.go
+++ main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"sync"
+	"time"
+
+	linkedlist "github.com/hmcalister/Go-DSA/list/LinkedList"
+	"github.com/hmcalister/TeaTimer/internal/timerdata"
+)
+
+func main() {
+
+	var timerListMutex sync.RWMutex
+	allTimers := linkedlist.New[*timerdata.TimerData]()
+	globalTimer := time.NewTicker(time.Second)
+	go func() {
+		for range globalTimer.C {
+			timerListMutex.RLock()
+			linkedlist.ForwardApply(allTimers, func(item *timerdata.TimerData) {
+				item.UpdateChannel <- timerdata.UpdateMessageTick
+			})
+			timerListMutex.RUnlock()
+		}
+	}()
+
+	t1 := timerdata.NewTimer("Timer A", 7)
+	defer close(t1.UpdateChannel)
+	allTimers.Add(t1)
+
+	t2 := timerdata.NewTimer("Timer B", 7)
+	defer close(t2.UpdateChannel)
+	allTimers.Add(t2)
+
+	time.Sleep(3 * time.Second)
+	timerListMutex.Lock()
+	close(t1.UpdateChannel)
+	allTimers.RemoveAtIndex(0)
+	timerListMutex.Unlock()
+	for {
+	}
+}
