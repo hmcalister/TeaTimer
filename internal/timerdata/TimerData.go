0e595f81edd5d88b410371ce6d9c0fcf05bc5d88
Hayden McAlister <haydenmcalister49@gmail.com>
2024-08-21 00:31:10 +1200

    Add NewTimer function

--- internal/timerdata/TimerData.go
+++ internal/timerdata/TimerData.go
@@ -12,3 +12,20 @@ type TimerData struct {
 	UpdateChannel     chan TimerUpdateMessageEnum
 }
 
+// Creates and returns a new Timer.
+//
+// When finished with the timer, close the update channel to signal cleanup.
+func NewTimer(name string, duration int) *TimerData {
+	updateChannel := make(chan TimerUpdateMessageEnum)
+	t := &TimerData{
+		Name:              name,
+		TimerState:        TimerStateRunning,
+		InitialDuration:   duration,
+		RemainingDuration: duration,
+		UpdateChannel:     updateChannel,
+	}
+
+
+	return t
+}
+
