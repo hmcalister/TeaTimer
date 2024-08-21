0be803edf342311c4c4be1a0e2a9be1c575136aa
Hayden McAlister <haydenmcalister49@gmail.com>
2024-08-21 22:42:17 +1200

    Overhaul main to pass responsibility off to MainModel in TUI

--- main.go
+++ main.go
@@ -1,41 +1,17 @@
 package main
 
 import (
-	"sync"
-	"time"
+	"fmt"
+	"os"
 
-	linkedlist "github.com/hmcalister/Go-DSA/list/LinkedList"
-	"github.com/hmcalister/TeaTimer/internal/timerdata"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/hmcalister/TeaTimer/internal/tui"
 )
 
 func main() {
-
-	var timerListMutex sync.RWMutex
-	allTimers := linkedlist.New[*timerdata.TimerData]()
-	globalTimer := time.NewTicker(time.Second)
-	go func() {
-		for range globalTimer.C {
-			timerListMutex.RLock()
-			linkedlist.ForwardApply(allTimers, func(item *timerdata.TimerData) {
-				item.UpdateChannel <- timerdata.UpdateMessageTick
-			})
-			timerListMutex.RUnlock()
-		}
-	}()
-
-	t1 := timerdata.NewTimer("Timer A", 7)
-	defer close(t1.UpdateChannel)
-	allTimers.Add(t1)
-
-	t2 := timerdata.NewTimer("Timer B", 7)
-	defer close(t2.UpdateChannel)
-	allTimers.Add(t2)
-
-	time.Sleep(3 * time.Second)
-	timerListMutex.Lock()
-	close(t1.UpdateChannel)
-	allTimers.RemoveAtIndex(0)
-	timerListMutex.Unlock()
-	for {
+	mainInterface := tui.NewMainModel()
+	if _, err := tea.NewProgram(mainInterface).Run(); err != nil {
+		fmt.Printf("error encountered during bubble tea program: %v", err)
+		os.Exit(1)
 	}
 }
