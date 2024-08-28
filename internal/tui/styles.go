8e3dbf0e87c1d4cd7a4d0113862fcbda942febd6
Hayden McAlister <haydenmcalister49@gmail.com>
2024-08-29 00:42:21 +1200

    Add various styles

--- internal/tui/styles.go
+++ internal/tui/styles.go
@@ -2,25 +2,36 @@ package tui
 
 import "github.com/charmbracelet/lipgloss"
 
-var (
-	mainContent = lipgloss.NewStyle().
-			Border(lipgloss.RoundedBorder()).
-			Width(80).
-			Height(30).
-			Padding(0, 5)
+const (
+	contentMinWidth  = 40
+	contentMinHeight = 5
+)
 
-	popupContent = lipgloss.NewStyle().
-			Border(lipgloss.RoundedBorder()).
-			Width(40).
-			Height(20).
-			Padding(0, 5)
+var (
+	mainContentStyle = lipgloss.NewStyle().
+				Border(lipgloss.RoundedBorder()).
+				Width(contentMinWidth).
+				Height(contentMinHeight).
+				Padding(0, 5)
+
+	progressBarStyle = lipgloss.NewStyle().
+				Width(contentMinWidth)
+
+	popupContentStyle = lipgloss.NewStyle().
+				Border(lipgloss.RoundedBorder()).
+				Align(lipgloss.Left).
+				Width(contentMinWidth/2).
+				Height(contentMinHeight/2).
+				Padding(0, 5)
 
 	formLabelStyle = lipgloss.NewStyle().
-			Width(40).
+			Width(contentMinWidth / 3).
 			Align(lipgloss.Left).
 			MarginRight(5)
 
 	formInputStyle = lipgloss.NewStyle().
-		// Width(20).
-		Align(lipgloss.Right)
+			Width(contentMinWidth/6).
+			Border(lipgloss.RoundedBorder()).
+			Align(lipgloss.Left).
+			Padding(0, 5)
 )
