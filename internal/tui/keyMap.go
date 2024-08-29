123aa755df18d94046cc1bf34f4fc60bb51f3067
Hayden McAlister <haydenmcalister49@gmail.com>
2024-08-29 22:23:38 +1200

    Add scroll and submit key maps

--- internal/tui/keyMap.go
+++ internal/tui/keyMap.go
@@ -3,9 +3,12 @@ package tui
 import "github.com/charmbracelet/bubbles/key"
 
 type keybindList struct {
-	exit key.Binding
-	quit key.Binding
-	add  key.Binding
+	exit       key.Binding
+	quit       key.Binding
+	add        key.Binding
+	scrollUp   key.Binding
+	scrollDown key.Binding
+	submit     key.Binding
 }
 
 func newKeybindList() *keybindList {
@@ -21,6 +24,18 @@ func newKeybindList() *keybindList {
 		add: key.NewBinding(
 			key.WithKeys("a"),
 			key.WithHelp("a", "New Timer"),
+		),
+		scrollUp: key.NewBinding(
+			key.WithKeys("k", "up", "shift+tab"),
+			key.WithHelp("up", "Scroll Up"),
+		),
+		scrollDown: key.NewBinding(
+			key.WithKeys("j", "down", "tab"),
+			key.WithHelp("down", "Scroll Down"),
+		),
+		submit: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter", "Submit"),
 		),
 	}
 }
